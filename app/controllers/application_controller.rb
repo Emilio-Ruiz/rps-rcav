f33384ff6be2037a111f8cc8538c9ba4bd167181
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-02-27 22:18:43 +0000

    newscomit

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,12 +3,31 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
+
+  def homepage 
+    render({ :template => "game_templates/rules.erb"})
+  end
   
   def play_rock
   
     #redirect_to("https://www.wikipedia.org")
 
     render({ :template => "game_templates/user_rock.html.erb"})
-
   end
+  
+  def play_paper 
+
+      comp_move = ["rock", "paper", "scissors"].sample 
+
+      if comp_move == "rock"
+        We tied!
+      elsif comp_move == "paper"
+        We lost!
+      elsif comp_move == "scissors" 
+        We won!
+      end
+
+    render({ :template => "game_templates/user_paper.html.erb"})
+  end 
+
 end
