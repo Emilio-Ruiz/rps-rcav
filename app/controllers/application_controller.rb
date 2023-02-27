afc29429399ab3a363f1b408f6d7656544899265
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-02-27 21:16:19 +0000

    yes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,16 +3,12 @@ class ApplicationController < ActionController::Base
 
   # Add your actions below this line
   # ================================
-  def homepage
-    render({ :template => "game_templates/rules.html.erb" })
-  end
-
+  
   def play_rock
-    # write a ton of code
+  
+    #redirect_to("https://www.wikipedia.org")
 
-    #redirect_to("http://www.wikipedia.org")
-    #render({:html => "<h1>Howdy, World!</h1>".html_safe })
+    render({ :template => "game_templates/user_rock.html.erb"})
 
-    render({ :template => "game_templates/user_rock.html.erb" })
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do 
-
-  get("/", {:controller => "application", :action => "homepage"})
-
-  get("/rock", {:controller => "application", :action => "play_rock"})
-
+  get("/rock", { :controller => "application", :action => "play_rock"})
 end
 
 
