a6137c0a1b1cf6e7520a1aacc44440c5ce0bed72
Emilio Ruiz <ruiz.emilio144@gmail.com>
2023-02-28 17:12:36 +0000

    git

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,14 @@ class ApplicationController < ActionController::Base
   end
   
   def play_rock
+    @comp_move = ["rock", "paper", "scissors"].sample
+    if @comp_move == "rock"
+      @outcome = "won"
+    elsif @comp_move == "paper"
+      @outcome = "tied"
+    elsif @comp_move ==     "scissors" 
+      @outcome = "lost"
+    end
   
     #redirect_to("https://www.wikipedia.org")
 
@@ -16,18 +24,30 @@ class ApplicationController < ActionController::Base
   end
   
   def play_paper 
-
-      @comp_move = ["rock", "paper", "scissors"].sample 
-
+      @comp_move = ["rock", "paper", "scissors"].sample
       if @comp_move == "rock"
         @outcome = "won"
       elsif @comp_move == "paper"
         @outcome = "tied"
-      elsif @comp_move == "scissors" 
+      elsif @comp_move ==     "scissors" 
         @outcome = "lost"
       end
 
-    render({ :template => "game_templates/user_paper.html.erb"})
+      render({ :template => "game_templates/user_paper.html.erb"})
+    end
+
+  def play_scissors
+        @comp_move = ["rock", "paper", "scissors"].sample 
+        if @comp_move == "rock"
+          @outcome = "lost"
+        elsif @comp_move =="paper"
+          @outcome = "won"
+        elsif @comp_move == "scissors"
+          @outcome = "tied"
+      end 
+
+  
+    render({ :template => "game_templates/user_scissors.html.erb"})
   end 
 
 end
